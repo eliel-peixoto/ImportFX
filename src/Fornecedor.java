1fb9dd7b0f1f26cc4c59b4c62eb653a086c223ed
Artur Cândido <arturcdn10@gmail.com>
2026-09-23 15:08:05 -0300

    Update Fornecedor.java
    só coloquei descrições para organização

--- src/Fornecedor.java
+++ src/Fornecedor.java
@@ -1,18 +1,20 @@
 //import java.util.ArrayList;
 
 public class Fornecedor {
+    //atributos
     String nome;
-    String PdOrigem;
-    String moedaPadr;
+    String PdOrigem;  //Pais de Origem
+    String moedaPadr; //moeda do Pais de Origem (vamos usar a API)
     //<ArrayList> produto;
 
+    //construtor
     public Fornecedor(String nome, String PdOrigem, String moedaPadr) {
         this.nome = nome;
         this.PdOrigem = PdOrigem;
         this.moedaPadr = moedaPadr;
     }
 
-
+    //getters e setters
     public String getNome() {
         return this.nome;
     }
