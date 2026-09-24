3179f6ef762c2d8e5e630a48745b5ccd24cfbd7e
Artur Cândido <arturcdn10@gmail.com>
2026-09-24 11:46:24 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Cliente c1 = new Cliente("JJ", 123456, "asdfghjkl");
+        Cliente c2 = new Cliente("AA", 67890, "qwertyuiop");
+        
+        Fornecedor f1 = new Fornecedor("Rock Lee", "Japão", "Yen");
+        Fornecedor f2 = new Fornecedor("Zip Zlap", "Planeta Zinx", "Flux");
+        
+        CategoriaProduto cp1 = new CategoriaProduto(10, f1, "shuriken", 20, 10, 1);
+        CategoriaProduto cp2 = new CategoriaProduto(20, f2, "arma lazer", 10, 100, 10000);
+        
+        f1.adicionar(cp1);
+        f2.adicionar(cp2);
+        
     }
 }
