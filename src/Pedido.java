93365e15c4c803c80b6149be8152cd85456faf2f
Artur Cândido <arturcdn10@gmail.com>
2026-09-24 09:33:50 -0300

    Update Pedido.java

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,47 +1,51 @@
-public class Pedido {
-    private Cliente cliente;
-    private Fornecedor fornecedor;
-    private int codigo;
-    private String status;
 
-    public Pedido(Cliente cliente, Fornecedor fornecedor, int codigo, String status) {
-        this.cliente = cliente;
-        this.fornecedor = fornecedor;
-        this.codigo = codigo;
-        this.status = status;
-    }
-
-
-    public Cliente getCliente() {
-        return this.cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public Fornecedor getFornecedor() {
-        return this.fornecedor;
-    }
-
-    public void setFornecedor(Fornecedor fornecedor) {
-        this.fornecedor = fornecedor;
-    }
-
-    public int getCodigo() {
-        return this.codigo;
-    }
-
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+public abstract class Produto {
+	//atributos
+	private int preco;
+	private Fornecedor fornecedor;
+	
+	//construtor
+	public Produto(int preco, Fornecedor fornecedor){
+		setPreco(preco);
+		setFornecedor(fornecedor);
+	}
+	
+	//getters e setters
+	public int getPreco() {
+		return preco;
+	}
+	public void setPreco(int preco) {
+		this.preco = preco;
+	}
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Produto [preco: " + this.getPreco() + "; Fornecedor: " + this.getFornecedor().getNome() + "]";
+	}
+	
+	//metodo equals()
+	@Override
+	public boolean equals(Object obj) {
+		boolean resultado = false;
+		//checagem se o objeto é o mesmo produto, nulo ou da mesma classe
+		if(this == obj) return true;
+		if(obj == null && this.getClass() != obj.getClass()) return false;
+		//cast (pq Object tem que ser da classe Produto)
+		Produto o = (Produto) obj;
+		
+		//checagem real da classe
+		if(this.getPreco() == o.getPreco() && this.getFornecedor() == o.getFornecedor()) {
+			resultado = true;
+		}
+		return resultado;
+	}
+	
 
 }
