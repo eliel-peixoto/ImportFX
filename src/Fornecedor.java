5cf62c749f539fe6692ab72078fe76bdec335133
Vitor Miguel <vitorazevedovmpa02@gmail.com>
2026-09-23 22:07:29 -0300

    Adição do array list, de mais um contrutor completo, dos getters, setters e métodos de manipulação do array list e dos métodos toString e equals (e o hashCode)

--- src/Fornecedor.java
+++ src/Fornecedor.java
@@ -1,19 +1,40 @@
-//import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class Fornecedor {
     //atributos
-    String nome;
-    String PdOrigem;  //Pais de Origem
-    String moedaPadr; //moeda do Pais de Origem (vamos usar a API)
-    //<ArrayList> produto;
+    private String nome;
+    private String pdOrigem;  //Pais de Origem
+    private String moedaPadr; //moeda do Pais de Origem (vamos usar a API)
+    private ArrayList<Produto> produto;
 
-    //construtor
-    public Fornecedor(String nome, String PdOrigem, String moedaPadr) {
-        this.nome = nome;
-        this.PdOrigem = PdOrigem;
-        this.moedaPadr = moedaPadr;
+    //construtor:
+
+    //incompleto
+    public Fornecedor(){
+        this.produto = new ArrayList<>();
     }
 
+    //completo mas sem lista
+    public Fornecedor(String nome, String pdOrigem, String moedaPadr) {
+        this.nome = nome;
+        this.pdOrigem = pdOrigem;
+        this.moedaPadr = moedaPadr;
+        this.produto = new ArrayList<>();
+    }
+
+    //completo e com lista
+    public Fornecedor(String nome, String pdOrigem, String moedaPadr, ArrayList<Produto> produto) {
+        this.nome = nome;
+        this.pdOrigem = pdOrigem;
+        this.moedaPadr = moedaPadr;
+        if (produto != null) {
+            this.produto = produto;
+        } else {
+            this.produto = new ArrayList<>();
+        }
+    }
+    
     //getters e setters
     public String getNome() {
         return this.nome;
@@ -23,12 +44,12 @@ public class Fornecedor {
         this.nome = nome;
     }
 
-    public String getPdOrigem() {
-        return this.PdOrigem;
+    public String getpdOrigem() {
+        return this.pdOrigem;
     }
 
-    public void setPdOrigem(String PdOrigem) {
-        this.PdOrigem = PdOrigem;
+    public void setpdOrigem(String pdOrigem) {
+        this.pdOrigem = pdOrigem;
     }
 
     public String getMoedaPadr() {
@@ -37,6 +58,50 @@ public class Fornecedor {
 
     public void setMoedaPadr(String moedaPadr) {
         this.moedaPadr = moedaPadr;
+    }
+    public ArrayList<Produto> getProduto() {
+        return produto;
+    }
+
+    public void setProduto(ArrayList<Produto> produto) {
+        this.produto = produto;
+    }
+
+    //adicionar e remover produtos
+    public void adicionarProduto(Produto p) {
+        if (p != null && !this.produto.contains(p)) {
+            this.produto.add(p);
+        }
+    }
+
+    public void removerProduto(Produto p) {
+        this.produto.remove(p);
+    }
+
+    //Sobrecrevendo a classe object
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fornecedor que = (Fornecedor) o;
+        return Objects.equals(nome, que.nome) &&
+               Objects.equals(pdOrigem, que.pdOrigem) &&
+               Objects.equals(moedaPadr, que.moedaPadr);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, pdOrigem, moedaPadr);
+    }
+
+    @Override
+    public String toString() {
+        return "Fornecedor{" +
+                "nome='" + nome + '\'' +
+                ", pdOrigem='" + pdOrigem + '\'' +
+                ", moedaPadr='" + moedaPadr + '\'' +
+                ", qtdProdutos=" + (produto != null ? produto.size() : 0) +
+                '}';
     }
 
 }
